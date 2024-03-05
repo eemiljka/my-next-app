b4b2ef263f0eca48460af6826e1235b7db8b100e
eemiljka <eemiljka@metropolia.fi>
2024-03-05 15:15:38 +0200

    pages added

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">
+        Use one of these: Profile or Upload
+      </h1>
+    </main>
+  );
+};
+
+export default Page;
